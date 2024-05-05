767784b93846fce84f09b7ee9950788b21604cbc
arnobarzan <arno.barzan@pxl.be>
2024-05-05 22:58:25 +0200

    Initial commit

---
+++ FilesAndMultiWindows/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+
+namespace FilesAndWIndows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _selectedFolder;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+}
